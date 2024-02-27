907cd2a59d5b3888480849e175b36da042045dd2
Sworq <ikosachev0@gmail.com>
2024-02-27 19:06:05 +0500

    zadacha1

--- zadachi.php
+++ zadachi.php
@@ -1,16 +1,67 @@
 <?php
-
+//создание класса
 class figure {
-	public $type;
-	public $color;
-	public function Info($type, $color) {
+	
+	//инициализация свойств
+	protected $type;
+	protected $color;
+
+	//конструктор класса
+	public function __construct($type, $color) {
 		$this -> type = $type;
 		$this -> color = $color; 
 	}
+
+	//вывод на экран
+	public function displayInfo() {
+		echo $this->type," ", $this->color, " ";
+	}
 }
 
-$a = new figure("квадрат", "красный");
-echo $a->type;
-echo $a->color;
+
+//класс наследник круг
+class circle extends figure {
+	
+	//вывод площади круга
+	public function CircleArea($radius) {
+		return pi() * $radius * $radius . "<br>";
+	}
+
+}
+
+//класс наследник квадрат
+class square extends figure {
+	
+	//вывод площади квадрата
+	public function SquareArea($a) {
+		return $a * $a . "<br>";
+	}
+
+}
+
+//класс наследник треугольник
+class triangle extends figure {
+	
+	//вывод площади треугольника
+	public function TriangleArea($height, $a) {
+		return 0.5 * $height * $a . "<br>";
+	}
+
+}
+
+
+
+//создание объектов
+$square = new square("square", "red");
+$square->displayInfo();
+echo $square->SquareArea(4);
+
+$circle = new circle("circle", "blue");
+$circle->displayInfo();
+echo $circle->CircleArea(4);
+
+$triangle = new triangle("triangle", "green");
+$triangle->displayInfo();
+echo $triangle->TriangleArea(4, 6);
 
 ?>
